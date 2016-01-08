5dbe2936d47ce535f509ac09b74b5abb247d8c29
aitkachuk home <you@example.com>
2016-01-08 14:44:36 +0200

    second

---
+++ footer.php
@@ -0,0 +1,21 @@
+		<!-- Footer -->
+			<div id="footer">
+
+				<!-- Copyright -->
+					<ul class="copyright">
+						<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="<?php echo get_template_directory_uri() ?>/assets/js/jquery.min.js"></script>
+			<script src="<?php echo get_template_directory_uri() ?>/assets/js/jquery.scrolly.min.js"></script>
+			<script src="<?php echo get_template_directory_uri() ?>/assets/js/jquery.scrollzer.min.js"></script>
+			<script src="<?php echo get_template_directory_uri() ?>/assets/js/skel.min.js"></script>
+			<script src="<?php echo get_template_directory_uri() ?>/assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="<?php echo get_template_directory_uri() ?>/assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="<?php echo get_template_directory_uri() ?>/assets/js/main.js"></script>
+
+	</body>
+</html>
